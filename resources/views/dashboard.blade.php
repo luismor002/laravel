c20265c14df071b55d040404c193c3223f6020c6
luisfer002 <lfernando.morales@edae.com.mx>
2023-06-02 11:48:52 -0600

    avanze con cuadros color degradado acomodados

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,16 +7,15 @@
     <div class="flex flex-wrap w-full bg-gray-800  ">
         
         <div class="flex flex-wrap bg-green-700 h-[300px] w-full sm:w-1/2 p-5">
-             
-            <div class="bg-red-500 w-full sm:w-1/3"> sdfdfsf</div>
-            <div class="bg-cyan-700 w-full sm:w-1/3">2</div>
-            <div class="bg-yellow-500  w-full sm:w-1/3">3</div>   
+            <div class="bg-red-500 w-full bg-gradient-to-r from-cyan-500 to-blue-700"> sdfdfsf</div>
+            <div class="bg-cyan-700 w-full sm:w-1/2">2</div>
+            <div class="bg-yellow-500  w-full sm:w-1/2">3</div>   
         </div>
     
-        <div class="flex flex-wrap bg-white h-[300px] w-full sm:w-1/2 border-4 p-5 border-black">
-            <div class="bg-green-500 w-full sm:w-1/3">1</div>
-            <div class="bg-red-600 w-full sm:w-1/3">2</div>
-            <div class="bg-pink-600 w-full sm:w-1/3">sm:w-1/23</div>
+        <div class="flex flex-wrap bg-gradient-to-r from-pink-500 to-violet-500 h-[300px] w-full sm:w-1/2 border-4 p-5 border-black">
+            <div class="bg-green-500 w-full sm:w-1/2 ">1</div>
+            <div class="bg-red-600 w-full sm:w-1/2 ">2</div>
+            <div class="bg-pink-600 w-full bg-gradient-to-r from-yellow-500 to-orange-500">3</div>
         </div>
    
     </div>
